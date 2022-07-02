ae1a8076aa7814bdc9063185160638646f810466
Scott Wilson <swrs@hotmail.co.uk>
2022-07-03 00:28:24 +0100

    added media queries to make the about section responsive

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,20 @@ import "./index.css";
 function About() {
   return (
     <div className="about-wrapper">
-      <div className="about">About</div>
+      <div className="about">
+        About
+        <div className="about-content">
+          <div className="about-image"></div>
+          <p className="about-text">
+            A motivated software engineer, seeking an environment to get stuck
+            in to and use my problem solving mindset and skills I have learned
+            during my time at School of Code. From previous employments, I have
+            developed soft skills that I believe will help me make a difference
+            in the tech industry - Communication, Leadership and Adaptability
+            are among these.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -5,7 +5,7 @@ function Welcome() {
   return (
     <div className="Welcome-container">
       <h1>Welcome To My Portfolio</h1>
-      <h2>Hi I'm Scott Andrew Wilson</h2>
+      <h2>Hi, I'm Scott Andrew Wilson</h2>
       <h3>A Software Engineer</h3>
       <h4>Based In The East Midlands, UK</h4>{" "}
     </div>
