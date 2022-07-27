6abef265f6577e2a7eeb20f61020ffcd4f6d8d8a
Scott Wilson <swrs@hotmail.co.uk>
2022-07-27 14:18:19 +0100

    removed Testimonials from the site

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import Testimonials from "./components/Testimonials";
 import Welcome from "./components/Welcome";
 
 function App() {
@@ -18,7 +17,6 @@ function App() {
           <About />
           <Projects />
           <Skills />
-          <Testimonials />
           <Contact />
         </div>
       </div>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,7 +10,6 @@ function Navbar() {
       <p className="linkButton">About</p>
       <p className="linkButton">Projects</p>
       <p className="linkButton">Skills</p>
-      <p className="linkButton">Testimonials</p>
       <p className="linkButton">Contact</p>
     </div>
   );
