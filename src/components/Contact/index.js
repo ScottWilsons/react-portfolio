cab9e71e5f67d88dc287655abb8366fad9726687
Scott Wilson <swrs@hotmail.co.uk>
2022-08-04 15:17:16 +0100

    added contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,6 +6,38 @@ function Contact() {
     <div id="contact" className="contact-wrapper">
       <div className="contact">
         <h2 className="contact-title">Contact</h2>
+        <div className="contact-content">
+          <form
+            action="https://formsubmit.co/88ffc94f0170df701aa60e5b85f4d42f"
+            method="POST"
+          >
+            <input
+              type="email"
+              name="email"
+              placeholder="Your email"
+              required
+            ></input>
+            <input
+              type="text"
+              name="Name"
+              placeholder="Your name"
+              required
+            ></input>
+            <input
+              type="text"
+              name="Message"
+              placeholder="Your message"
+              required
+            ></input>
+            <input
+              type="hidden"
+              name="_subject"
+              value="New Message From Portfolio"
+              required
+            ></input>
+            <button type="submit">Send</button>
+          </form>
+        </div>
       </div>
     </div>
   );
