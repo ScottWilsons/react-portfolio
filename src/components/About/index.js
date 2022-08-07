ef05cd85b1116e1a380cbb0de7cd354bc25dbe63
Scott Wilson <swrs@hotmail.co.uk>
2022-08-07 22:04:55 +0100

    final commit.. for now

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,14 +8,16 @@ function About() {
         <h2 className="about-title">About</h2>
         <div className="about-content">
           <div className="about-image"></div>
-          <p className="about-text">
-            A motivated software engineer, seeking an environment to get stuck
-            in to and use my problem solving mindset and skills I have learned
-            during my time at School of Code. From previous employments, I have
-            developed soft skills that I believe will help me make a difference
-            in the tech industry - Communication, Leadership and Adaptability
-            are among these.
-          </p>
+          <div className="about-text-background">
+            <p className="about-text">
+              A motivated software engineer, seeking an environment to get stuck
+              in to and use my problem solving mindset and skills I have learned
+              during my time at School of Code. From previous employments, I
+              have developed soft skills that I believe will help me make a
+              difference in the tech industry - Communication, Leadership and
+              Adaptability are among these.
+            </p>
+          </div>
         </div>
       </div>
     </div>
